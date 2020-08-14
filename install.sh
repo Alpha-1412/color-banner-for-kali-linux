17385373722ec185f885b2d3df9ae6283700fbc8
Alpha-1412 <69677694+Alpha-1412@users.noreply.github.com>
2020-08-14 22:10:32 +0530

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,17 @@
+# #dependecy packets
+#sudo apt install figlet -y && apt install lolcat -y
+
+# #clone this repo for figlet-fonts
+# git clone https://github.com/xero/figlet-fonts.git
+
+# #move font to the figlet font directory
+# mv figlet-fonts/* /usr/share/figlet/
+
+# #remove the cloned repo from system
+# rm -rf figlet-fonts
+
+sudo cp ~/.bashrc ~/.bashrc_old;
+
+sudo cp ./.bashrc ~/.bashrc;
+
+
---
+++ uninstall.sh
@@ -0,0 +1,3 @@
+rm -f ~/.bashrc 
+cp .bashrc_old ~/.bashrc
+
